37449b9983c8b2d99e1743fcccf9cb3add2fb905
rayko33 <rayko033@hotmail.com>
2023-07-03 16:50:54 -0400

    dropdown eliminado

--- resources/views/coach/partial/navbar.blade.php
+++ resources/views/coach/partial/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg " style="background: linear-gradient(to right, #00A388, #3BBDA6, #76D7C4);">
     <div class="container-fluid">
       
-      <a class="navbar-brand text-white" href="{{route('coachdashbord')}}">{{config('app.name')}} - Dashboard</a>
+      <a class="navbar-brand text-white" href="{{route('coachdashbord')}}">{{config('app.name')}} - {{$title}}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -14,22 +14,10 @@
             <a class="nav-link text-white" href="{{route('clientes')}}">Clientes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Citas</a>
+            <a class="nav-link text-white" href="{{route('sessions')}}">sesiones</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-      </div>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="{{route('outcoach')}}">Logout</a></li>
-              <li><a class="dropdown-item" href="#">Perfil</a></li>
-            </ul>
+            <a class="nav-link text-danger" href="{{route('outcoach')}}">Cerrar sesión</a>
           </li>
         </ul>
       </div>
