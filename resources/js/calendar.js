511c90cc7eb6de5efc7404d9b39f34896fc2debf
rayko33 <rayko033@hotmail.com>
2023-07-04 14:49:50 -0400

    Funciones desabilitar y habititar updatemodal

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -25,25 +25,13 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
     right: 'prev,next' 
   },
   events: `http://127.0.0.1:8000/trainingsessions/`,
-  dateClick: function(info) {
-    var day = info.date.getDay();
-    
-    // Verifica si el día no está permitido (por ejemplo, domingo)
-    if (day === 0) {
-      // No permite la selección
-      return false;
-    }
-    
-    // Permite la selección para los demás días
-    return true;
-  },
   eventClick: function(info) {
       if(info.event.extendedProps.status=='finalizada'||info.event.extendedProps.status=='cancelada'){
-          var modal = document.getElementById('exampleModal');
+          var modal = document.getElementById('modaleventClick');
 
         // Obtener todos los elementos dentro del modal que se pueden deshabilitar
-          var elements = modal.querySelectorAll('input, select, textarea, button:not(#close,)');
-          alert(info.event.textColor)
+          var elements = modal.querySelectorAll('input, select, textarea, button:not(#close)');
+          
           
         // Deshabilitar todos los elementos dentro del modal
           elements.forEach(function (element) {
@@ -52,13 +40,12 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
             }
           
           });
-        $('#exampleModal').modal('show')
+        $('#modaleventClick').modal('show')
       }
+      $('#titleEvent').text(info.event.title)
+      $('#modaleventClick').modal('show')
       
-      // Obtener el modal por su ID
-      $('#exampleModal').modal('show')
-    // change the border color just for fun
-    info.el.style.borderColor = 'red';
+    
   },
   dateClick: function(info) {
     
@@ -93,7 +80,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //busqueda global dropdown clientes
-$("#selectClientes").select2();
+$("#selectClientes").select2({
+  width: '100%'
+});
 
 //Evento click filtrar cita por usuario tabla
 $("#tableBody").on("click", "tr", function () {
@@ -123,60 +112,206 @@ $("#tableBody").on("click", "tr", function () {
 
 });
 
-//evento change filtrar sitas por clientes
+//evento change filtrar citas por clientes
 $("#selectClientes").on("change", function () {
-  var id = $(this).val();
-  console.log("ID: " + id);
-  calendar.setOption('events', `http://127.0.0.1:8000/trainingsessions/member/${id}`);
-  calendar.refetchEvents();
+  var id = $(this).val()
+  console.log("ID: " + id)
+  calendar.setOption('events', `http://127.0.0.1:8000/trainingsessions/member/${id}`)
+  calendar.refetchEvents()
 });
 
-//evento click habilitar inputs y botones modal cerrar header
+//evento click habilitar inputs y botones modal cerrar header (modal agregar cita)
 $(".btn-close").on("click", function () {
 
-  var modal = document.getElementById('exampleModal');
-  var elements = modal.querySelectorAll('input, select, textarea, button');
+  var modal = document.getElementById('exampleModal')
+  var elements = modal.querySelectorAll('input, select, textarea, button')
 
   elements.forEach(function (element) {
     if (!element.classList.contains('btn-close')) {
-      element.disabled = false;
+      element.disabled = false
     }
   
   });
 });
 
-//Evento click habilitar inputs y botnes modal al cerrar con boton footer
+//Evento click habilitar inputs y botnes modal al cerrar con boton footer (Agregar citas)
 $("#btn-close").on("click", function () {
 
-  var modal = document.getElementById('exampleModal');
-  var elements = modal.querySelectorAll('input, select, textarea, button');
+  var modal = document.getElementById('exampleModal')
+  var elements = modal.querySelectorAll('input, select, textarea, button')
 
   elements.forEach(function (element) {
     if (!element.classList.contains('btn-close')) {
-      element.disabled = false;
+      element.disabled = false
     }
   
   });
 });
-//Obtener elementos del DOM con id start y end (datetime-local)
-let datetime1 = document.getElementById('start')
-let datetime2 = document.getElementById('end')
-//evento input limitar end segun datetime-local start
-datetime1.addEventListener('input', function() {
-  var datetime1Value = new Date(datetime1.value);
-  var datetime2Value = new Date(datetime2.value);
+
+//Habilidar botones al hacer click fuera del modal
+$('#exampleModal').on('hidden.bs.modal', function () {
+  // Habilitar los inputs aquí
+  var modal = $('#exampleModal')
+  var elements = modal.find('input, select, textarea, button').not('.btn-close')
+
+  elements.prop('disabled', false)
+});
+
+//evento click habilitar inputs y botones modal cerrar header (Update citas)
+$(".btn-close").on("click", function () {
+
+  var modal = document.getElementById('modaleventClick')
+  var elements = modal.querySelectorAll('input, select, textarea, button')
+
+  elements.forEach(function (element) {
+    if (!element.classList.contains('btn-close')) {
+      element.disabled = false
+    }
   
-  if(datetime1Value>datetime2Value && datetime2Value!=null){
-    datetime2.value = datetime1.value;
+  });
+});
+//evento click habilitar inputs y botones modal cerrar footer (Update citas)
+$("#btn-close-update").on("click", function () {
+
+  var modal = document.getElementById('modaleventClick')
+  var elements = modal.querySelectorAll('input, select, textarea, button')
+
+  elements.forEach(function (element) {
+    if (!element.classList.contains('btn-close')) {
+      element.disabled = false
+    }
+  
+  });
+});
+
+//Habilidar botones al hacer click fuera del modal
+$('#modaleventClick').on('hidden.bs.modal', function () {
+  // Habilitar los inputs aquí
+  var modal = $('#modaleventClick')
+  var elements = modal.find('input, select, textarea, button').not('.btn-close')
+
+  elements.prop('disabled', false)
+});
+
+//Obtener elementos del DOM con id start y end (datetime-local)
+let startDate = document.getElementById('start')
+let endDate = document.getElementById('end')
+//evento input limitar end segun datetime-local start
+startDate.addEventListener('input', function() {
+  var startDateValue = new Date(startDate.value)
+  var endDateValue = new Date(endDate.value)
+  
+  if(startDateValue>endDateValue && endDateValue!=null){
+    endDate.value = startDate.value
   }
 });
 //Evento input limitar datetime-local y mostrar alert de fecha invalida 
-datetime2.addEventListener('input', function() {
-  var datetime1Value = new Date(datetime1.value);
-  var datetime2Value = new Date(datetime2.value);
+endDate.addEventListener('input', function() {
+  var startDateValue = new Date(startDate.value)
+  var endDateValue = new Date(endDate.value)
   
-  if (datetime1Value > datetime2Value) {
+  if (startDateValue > endDateValue) {
     alert('la hora de termino no puede ser menor a la hora de inicio')
-    datetime2.value = datetime1.value;
+    endDate.value = startDate.value
+    return
   }
+  if (startDateValue == endDateValue) {
+    alert('la hora de termino no puede ser igual a la hora de inicio')
+    endDate.value = startDate.value
+    return
+  }
+});
+
+
+// Obtén el elemento del radio check de opción grupal
+let opcionGrupal = document.getElementById('radioGrupal');
+// Obtén el elemento del grupo de cantidad de miembros
+let cantidadMiembrosGroup = document.getElementById('membersContainer');
+// Obtén el elemento del radio check de opción individual
+let opcionIndividual = document.getElementById('radioIndividual');
+// Agrega un evento change al radio check grupal
+opcionGrupal.addEventListener('change', function() {
+  // Si se selecciona la opción grupal, muestra el grupo de cantidad de miembros
+  if (opcionGrupal.checked) {
+    cantidadMiembrosGroup.style.display = 'block';
+  } else {
+    // Si se selecciona la opción individual, oculta el grupo de cantidad de miembros
+    cantidadMiembrosGroup.style.display = 'none';
+  }
+});
+// Agrega un evento change al radio check individual
+opcionIndividual.addEventListener('change', function() {
+  // Si se selecciona la opción individual, El elemento cantidad de miembros se oculta
+  if (opcionIndividual.checked) {
+    cantidadMiembrosGroup.style.display = 'none';
+  }
+});
+
+$('#exampleModal').on('hidden.bs.modal', function () {
+  // Restablecer los valores de los campos del formulario
+  $(this).find(':input').val('');
+});
+
+// Obtén el elemento del radio check de opción grupal
+let opcionGrupalUpdate = document.getElementById('radioGrupalUpdate');
+// Obtén el elemento del grupo de cantidad de miembros
+let cantidadMiembrosGroupUpdate = document.getElementById('membersContainerUpdate');
+// Obtén el elemento del radio check de opción individual
+let opcionIndividualUpdate = document.getElementById('radioIndividualUpdate');
+// Agrega un evento change al radio check grupal
+opcionGrupalUpdate.addEventListener('change', function() {
+  // Si se selecciona la opción grupal, muestra el grupo de cantidad de miembros
+  if (opcionGrupalUpdate.checked) {
+    cantidadMiembrosGroupUpdate.style.display = 'block';
+  } else {
+    // Si se selecciona la opción individual, oculta el grupo de cantidad de miembros
+    cantidadMiembrosGroupUpdate.style.display = 'none';
+  }
+});
+// Agrega un evento change al radio check individual
+opcionIndividualUpdate.addEventListener('change', function() {
+  // Si se selecciona la opción individual, El elemento cantidad de miembros se oculta
+  if (opcionIndividual.checked) {
+    cantidadMiembrosGroupUpdate.style.display = 'none';
+  }
+});
+
+$('#modaleventClick').on('hidden.bs.modal', function () {
+  // Restablecer los valores de los campos del formulario
+  $(this).find(':input').val('');
+});
+
+//Obtener elementos del DOM con id start y end (datetime-local)
+let startUpdate = document.getElementById('start-update')
+let endUpdate = document.getElementById('end-update')
+//evento input limitar end segun datetime-local start
+startUpdate.addEventListener('input', function() {
+  var startDateValue = new Date(startUpdate.value)
+  var endDateValue = new Date(endUpdate.value)
+  
+  if(startDateValue>endDateValue && endDateValue!=null){
+    endUpdate.value = startUpdate.value
+  }
+});
+//Evento input limitar datetime-local y mostrar alert de fecha invalida 
+endUpdate.addEventListener('input', function() {
+  var startDateValue = new Date(startUpdate.value)
+  var endDateValue = new Date(endUpdate.value)
+  
+  if (startDateValue > endDateValue) {
+    alert('la hora de termino no puede ser menor a la hora de inicio')
+    endUpdate.value = startUpdate.value
+    return
+  }
+  if (startDateValue.getTime()== endDateValue.getTime()) {
+    alert('la hora de termino no puede ser igual a la hora de inicio')
+    endUpdate.value = startUpdate.value
+    return
+  }
+});
+
+//busqueda global dropdown clientes
+$("#selectClientesUpdate").select2({
+  dropdownParent: $('#modaleventClick'),
+  width: '100%'
 });
