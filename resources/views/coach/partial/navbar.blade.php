718fd93f75e4014a418f0af0d4e15468e1e8d387
rayko33 <rayko033@hotmail.com>
2023-07-17 17:27:14 -0400

    titulo navbar dinamico

--- resources/views/coach/partial/navbar.blade.php
+++ resources/views/coach/partial/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg " style="background: linear-gradient(to right, #00A388, #3BBDA6, #76D7C4);">
     <div class="container-fluid">
       
-      <a class="navbar-brand text-white" href="{{route('coachdashbord')}}">{{config('app.name')}} - {{$title}}</a>
+      <a class="navbar-brand text-white" href="{{route('coachdashbord')}}">{{config('app.name')}} - @yield('section')</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
