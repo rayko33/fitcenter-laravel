d2809fad45d0261c2588d3503184138af8f9f624
rayko33 <rayko033@hotmail.com>
2023-07-19 13:42:46 -0400

    modificado

--- resources/views/coach/sessions.blade.php
+++ resources/views/coach/sessions.blade.php
@@ -65,11 +65,6 @@
         </div>
     </div>
 
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Launch demo modal
-    </button>
-
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
@@ -79,16 +74,17 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <form>
+            <form id="createSessionForm">
+              @csrf
               <div class="mb-3">
                 <label for="title" class="col-form-label">Titulo</label>
-                <input type="text" class="form-control" id="title">
+                <input type="text" class="form-control" id="title" name="title">
               </div>
               <div class="mb-3 form-inline">
                 <label for="start" class="col-form-label">Inicio</label>
-                <input type="datetime-local" class="form-control" id="start">
+                <input type="datetime-local" class="form-control" id="start" name="start">
                 <label for="end" class="col-form-label">Termino</label>
-                <input type="datetime-local" class="form-control" id="end">
+                <input type="datetime-local" class="form-control" id="end" name="end">
               </div>
               <div class="mb-3">
                 <label for="" class="col-form-label">Tipo sesion</label>
@@ -107,7 +103,36 @@
               </div>
               <div class="mb-3" style="display: none" id="membersContainer">
                 <label for="cantidadMiembros" >Cantidad de miembros</label>
-                <input type="number" class="form-control" id="cantidadMiembros">
+                <input type="number" class="form-control" id="cantidadMiembros" name="cantidadMiembros">
+              </div>
+              <div class="mb-3">
+                <label for="" class="col-form-label">Modalidad de la sesión</label>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="modo" id="online" value="online" checked>
+                  <label class="form-check-label" for="online">
+                    Online
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="modo" id="presencial" value="presencial">
+                  <label class="form-check-label" for="presencial">
+                    Presencial
+                  </label>
+                </div>
+              </div>
+             
+              <div class="mb-3" style="display: none" id="direccionContainer">
+                <label for="direccion" >Cantidad de miembros</label>
+                <input type="number" class="form-control" id="direccion" name="direccion">
+              </div>
+              <div class="mb-3">
+                <label for="selectClient">Clientes</label>
+                <select id="selectClient" name="selectClient" class="form-select">
+                  <option value="non" selected>Selecciona un cliente</option>
+                    @foreach (json_decode($clients) as $client)
+                        <option value="{{$client->id}}">{{$client->name}} {{$client->lastname}}</option>
+                    @endforeach
+                </select>
               </div>
               <div class="mb-3">
                 <label for="" class="col-form-label">Visibilidad</label>
@@ -126,23 +151,18 @@
               </div>
               
             </form>
+            <div class="modal-footer">
+              <button type="button" id='close' class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="button" id='crearCita' class="btn btn-success">Ingresar cita</button>
+            </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" id='close' class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-            <button type="button" id='crearCita' class="btn btn-success">Ingresar cita</button>
-          </div>
+          
         </div>
       </div>
     </div>
     <!-- -->
 
     <!--Modal eventClick -->
-
-
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modaleventClick">
-      Launch demo modal
-    </button>
-
 
     <div class="modal fade" id="modaleventClick" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
@@ -151,68 +171,71 @@
             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
+          
           <div class="modal-body">
-            <form action="">
-              <div class="mb-3">
-                <label for="titleEventupdate" class="col-form-label">Titulo</label>
-                <input type="text" class="form-control" id="titleEventupdate">
-                
-              </div>
-              <div class="mb-3 form-inline">
-                <label for="start-update" class="col-form-label">Inicio</label>
-                <input type="datetime-local" class="form-control" id="start-update">
-                <label for="end-update" class="col-form-label">Termino</label>
-                <input type="datetime-local" class="form-control" id="end-update">
-              </div>
-              <div class="mb-3">
-                <label for="" class="col-form-label">Tipo sesion</label>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="tipoSesion" id="radioIndividualUpdate" value="individual" checked>
-                  <label class="form-check-label" for="radioIndividualUpdate">
-                    Indivual
-                  </label>
+            <form id="sessionUpdateForm" action="">
+                <div class="mb-3">
+                  <label for="titleEventupdate" class="col-form-label">Titulo</label>
+                  <input type="text" class="form-control" id="titleEventupdate">
+                  
                 </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="tipoSesion" id="radioGrupalUpdate" value="grupal">
-                  <label class="form-check-label" for="radioGrupalUpdate">
-                    Grupal
-                  </label>
+                <div class="mb-3 form-inline">
+                  <label for="start-update" class="col-form-label">Inicio</label>
+                  <input type="datetime-local" class="form-control" id="start-update">
+                  <label for="end-update" class="col-form-label">Termino</label>
+                  <input type="datetime-local" class="form-control" id="end-update">
                 </div>
-              </div>
-              <div class="mb-3" style="display: none" id="membersContainerUpdate">
-                <label for="cantidadMiembros" >Cantidad de miembros</label>
-                <input type="number" class="form-control" id="cantidadMiembros">
+                <div class="mb-3">
+                  <label for="" class="col-form-label">Tipo sesion</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="tipoSesion" id="radioIndividualUpdate" value="individual" checked>
+                    <label class="form-check-label" for="radioIndividualUpdate">
+                      Indivual
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="tipoSesion" id="radioGrupalUpdate" value="grupal">
+                    <label class="form-check-label" for="radioGrupalUpdate">
+                      Grupal
+                    </label>
+                  </div>
+                </div>
+                <div class="mb-3" style="display: none" id="membersContainerUpdate">
+                  <label for="cantidadMiembros" >Cantidad de miembros</label>
+                  <input type="number" class="form-control" id="cantidadMiembros">
+                </div>
+                <div class="mb-3">
+                  <label for="selectClientesUpdate">Clientes</label>
+                  <select id="selectClientesUpdate" class="form-select">
+                    <option value="non" selected>Selecciona un cliente</option>
+                      @foreach (json_decode($clients) as $client)
+                          <option value="{{$client->id}}">{{$client->name}} {{$client->lastname}}</option>
+                      @endforeach
+                  </select>
               </div>
               <div class="mb-3">
-                <label for="selectClientesUpdate">Clientes</label>
-                <select id="selectClientesUpdate" class="form-select">
-                  <option value="non" selected>Selecciona un cliente</option>
-                    @foreach (json_decode($clients) as $client)
-                        <option value="{{$client->id}}">{{$client->name}} {{$client->lastname}}</option>
-                    @endforeach
-                </select>
-            </div>
-              <div class="mb-3">
-                <label for="" class="col-form-label">Visibilidad</label>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="visibilidad" id="radioIndividual" value="publica" checked>
-                  <label class="form-check-label" for="radioIndividual">
-                    Publica
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="visibilidad" id="radioGrupal" value="privada">
-                  <label class="form-check-label" for="radioGrupal">
-                    Privada
-                  </label>
-                </div>
+                  <label for="" class="col-form-label">Visibilidad</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="visibilidad" id="radioIndividual" value="publica" checked>
+                    <label class="form-check-label" for="radioIndividual">
+                      Publica
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="visibilidad" id="radioGrupal" value="privada">
+                    <label class="form-check-label" for="radioGrupal">
+                      Privada
+                    </label>
+                  </div>
               </div>
               
             </form>
           </div>
           <div class="modal-footer">
+            <button type="button" class="btn btn-danger" id="cancelsession">Cancelar</button>
             <button type="button" class="btn btn-warning">Actualizar</button>
             <button type="button" class="btn btn-secondary" id="btn-close-update" data-bs-dismiss="modal">Cerrar</button>
+            
           </div>
         </div>
       </div>
