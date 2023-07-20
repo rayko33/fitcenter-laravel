47170734639ba9ed96cc886e5a7d20bca95f337b
rayko33 <rayko033@hotmail.com>
2023-07-20 02:33:17 -0400

    navbar modificado

--- resources/views/client/partial/nav.blade.php
+++ resources/views/client/partial/nav.blade.php
@@ -1,27 +1,39 @@
 <nav class="navbar navbar-expand-lg " style="background: linear-gradient(to right, #00A388, #3BBDA6, #76D7C4);">
     <div class="container-fluid">
       
-      <a class="navbar-brand text-white" href="{{route('coachdashbord')}}">{{config('app.name')}} - Dashboard</a>
+      <a class="navbar-brand text-white" href="{{route('coachdashbord')}}">{{config('app.name')}} - @yield('section')</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-center"  id="navbarNav" >
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="{{route('coachdashbord')}}">Home</a>
+            <a class="nav-link active text-white" aria-current="page" href="{{route('dashbordclient')}}">Inicio</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="{{route('clientes')}}">Clientes</a>
+          <li class="nav-item dropdown">
+            <button class="btn dropdown-toggle text-white" data-bs-toggle="dropdown" aria-expanded="false">
+              Entrenadores
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Mis entrenadores</a></li>
+              <li><a class="dropdown-item " href="{{route('client.searchtrainers')}}">Buscar entrenadores</a></li>
+              
+            </ul>
           </li>
           <li class="nav-item">
             <a class="nav-link text-white" href="#">Citas</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+          <li class="nav-item dropdown">
+            <button class="btn dropdown-toggle text-white" data-bs-toggle="dropdown" aria-expanded="false">
+              Opciones
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Mi informacion</a></li>
+              <li><a class="dropdown-item text-danger" href="{{route('logoutclient')}}">Cerrar sesión</a></li>
+              
+            </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-danger" href="{{route('logoutclient')}}">Logout</a>
-          </li>
+        
         </ul>
       </div>
       
